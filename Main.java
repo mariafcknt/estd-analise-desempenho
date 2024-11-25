7ad29b81490d24a05e71f7861050ec52b68edde5
Camile Nolli <105087825+camilenolli@users.noreply.github.com>
2024-11-25 21:49:54 +0000

    redix sort

--- Main.java
+++ Main.java
@@ -1,43 +1,53 @@
 public class Main {
 
-        public static void main(String[] args) {
-            
-            //Mudar o tamanho do vetor aqui (10.000, 50.000 ou 100.000)
-            int[] vetor = new int[100000];
-            
-            for(int i = 0; i < vetor.length; i++){
-                vetor[i] = (int) (Math.random() * vetor.length);
-                System.out.println(vetor[i]);
-            }
-            
-            long inicio = System.currentTimeMillis();
-            long fim;
-      
-            int aux;
+    public static void main(String[] args) {
 
-            //Início so algoritmo do teste
-            
-            for(int i = 0; i < vetor.length; i++){ 
-                for(int j = 0; j < vetor.length - 1 ; j++){ 
-                    if (vetor[j] > vetor[j+1]){
-                        aux = vetor[j];
-                        vetor[j] = vetor[j+1];
-                        vetor[j+1] = aux;
-                    }
-                }
-            }
+        int[] vetor = new int[100000];
 
-            //Fim do algoritmo do teste
-            
-            System.out.println("Vetor ordenado");
-            for(int i = 0; i < vetor.length; i++){
-                System.out.println(vetor[i]);
-            }
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = (int) (Math.random() * vetor.length);
+        }
 
-            System.out.println();
-            
-            fim = System.currentTimeMillis();
-            System.out.println("Tamanho do vetor: " + (vetor.length));
-            System.out.println("Tempo em milisegundos: " + (fim-inicio));
+        long inicio = System.currentTimeMillis();
+        long fim;
+
+        radixSort(vetor);
+
+        fim = System.currentTimeMillis();
+        System.out.println("Tamanho do vetor: " + vetor.length);
+        System.out.println("Tempo em milisegundos: " + (fim - inicio));
+    }
+
+    public static void radixSort(int[] vetor) {
+        int max = vetor[0];
+        for (int i = 1; i < vetor.length; i++) {
+            if (vetor[i] > max) {
+                max = vetor[i];
+            }
+        }
+
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countingSort(vetor, exp);
         }
     }
+
+    public static void countingSort(int[] vetor, int exp) {
+        int[] output = new int[vetor.length];
+        int[] count = new int[10];
+
+        for (int i = 0; i < vetor.length; i++) {
+            count[(vetor[i] / exp) % 10]++;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = vetor.length - 1; i >= 0; i--) {
+            output[count[(vetor[i] / exp) % 10] - 1] = vetor[i];
+            count[(vetor[i] / exp) % 10]--;
+        }
+
+        System.arraycopy(output, 0, vetor, 0, vetor.length);
+    }
+}
