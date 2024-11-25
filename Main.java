5687bc088be5beef0c8c1785e8d932ef179b131e
Camile Nolli <105087825+camilenolli@users.noreply.github.com>
2024-11-25 21:45:49 +0000

    merge sort

--- Main.java
+++ Main.java
@@ -1,43 +1,53 @@
 public class Main {
 
-        public static void main(String[] args) {
-            
-            //Mudar o tamanho do vetor aqui (10.000, 50.000 ou 100.000)
-            int[] vetor = new int[100000];
-            
-            for(int i = 0; i < vetor.length; i++){
-                vetor[i] = (int) (Math.random() * vetor.length);
-                System.out.println(vetor[i]);
-            }
-            
-            long inicio = System.currentTimeMillis();
-            long fim;
-      
-            int aux;
+    public static void main(String[] args) {
 
-            //Início so algoritmo do teste
-            
-            for(int i = 0; i < vetor.length; i++){ 
-                for(int j = 0; j < vetor.length - 1 ; j++){ 
-                    if (vetor[j] > vetor[j+1]){
-                        aux = vetor[j];
-                        vetor[j] = vetor[j+1];
-                        vetor[j+1] = aux;
-                    }
-                }
-            }
+        int[] vetor = new int[50000];
 
-            //Fim do algoritmo do teste
-            
-            System.out.println("Vetor ordenado");
-            for(int i = 0; i < vetor.length; i++){
-                System.out.println(vetor[i]);
-            }
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = (int) (Math.random() * vetor.length);
+        }
 
-            System.out.println();
-            
-            fim = System.currentTimeMillis();
-            System.out.println("Tamanho do vetor: " + (vetor.length));
-            System.out.println("Tempo em milisegundos: " + (fim-inicio));
+        long inicio = System.currentTimeMillis();
+        long fim;
+
+        mergeSort(vetor);
+
+        fim = System.currentTimeMillis();
+        System.out.println("Tamanho do vetor: " + vetor.length);
+        System.out.println("Tempo em milisegundos: " + (fim - inicio));
+    }
+
+    public static void mergeSort(int[] vetor) {
+        if (vetor.length <= 1) return;
+
+        int meio = vetor.length / 2;
+        int[] left = new int[meio];
+        int[] right = new int[vetor.length - meio];
+
+        System.arraycopy(vetor, 0, left, 0, meio);
+        System.arraycopy(vetor, meio, right, 0, vetor.length - meio);
+
+        mergeSort(left);
+        mergeSort(right);
+
+        merge(vetor, left, right);
+    }
+
+    public static void merge(int[] vetor, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                vetor[k++] = left[i++];
+            } else {
+                vetor[k++] = right[j++];
+            }
+        }
+        while (i < left.length) {
+            vetor[k++] = left[i++];
+        }
+        while (j < right.length) {
+            vetor[k++] = right[j++];
         }
     }
+}
