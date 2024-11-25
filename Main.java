6475896fc49e0bacf44ad09ac66ad10736480638
Camile Nolli <105087825+camilenolli@users.noreply.github.com>
2024-11-25 21:52:51 +0000

    counting sort

--- Main.java
+++ Main.java
@@ -1,43 +1,42 @@
 public class Main {
 
-        public static void main(String[] args) {
-            
-            //Mudar o tamanho do vetor aqui (10.000, 50.000 ou 100.000)
-            int[] vetor = new int[100000];
-            
-            for(int i = 0; i < vetor.length; i++){
-                vetor[i] = (int) (Math.random() * vetor.length);
-                System.out.println(vetor[i]);
-            }
-            
-            long inicio = System.currentTimeMillis();
-            long fim;
-      
-            int aux;
+    public static void main(String[] args) {
 
-            //Início so algoritmo do teste
-            
-            for(int i = 0; i < vetor.length; i++){ 
-                for(int j = 0; j < vetor.length - 1 ; j++){ 
-                    if (vetor[j] > vetor[j+1]){
-                        aux = vetor[j];
-                        vetor[j] = vetor[j+1];
-                        vetor[j+1] = aux;
-                    }
-                }
-            }
+        int[] vetor = new int[100000];
 
-            //Fim do algoritmo do teste
-            
-            System.out.println("Vetor ordenado");
-            for(int i = 0; i < vetor.length; i++){
-                System.out.println(vetor[i]);
-            }
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = (int) (Math.random() * vetor.length);
+        }
 
-            System.out.println();
-            
-            fim = System.currentTimeMillis();
-            System.out.println("Tamanho do vetor: " + (vetor.length));
-            System.out.println("Tempo em milisegundos: " + (fim-inicio));
+        long inicio = System.currentTimeMillis();
+        long fim;
+
+        countingSort(vetor);
+
+        fim = System.currentTimeMillis();
+        System.out.println("Tamanho do vetor: " + vetor.length);
+        System.out.println("Tempo em milisegundos: " + (fim - inicio));
+    }
+
+    public static void countingSort(int[] vetor) {
+        int max = vetor[0];
+        for (int i = 1; i < vetor.length; i++) {
+            if (vetor[i] > max) {
+                max = vetor[i];
+            }
+        }
+
+        int[] count = new int[max + 1];
+        for (int i = 0; i < vetor.length; i++) {
+            count[vetor[i]]++;
+        }
+
+        int k = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                vetor[k++] = i;
+                count[i]--;
+            }
         }
     }
+}
