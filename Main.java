e184efc169e0caff34c0a47dd95c5089bf95db8a
Guilherme Raiser <105473776+GuilhermeRaiser@users.noreply.github.com>
2024-11-26 22:43:04 +0000

    selection-sort

--- Main.java
+++ Main.java
@@ -1,43 +1,49 @@
 public class Main {
 
-        public static void main(String[] args) {
-            
-            //Mudar o tamanho do vetor aqui (10.000, 50.000 ou 100.000)
-            int[] vetor = new int[100000];
-            
-            for(int i = 0; i < vetor.length; i++){
-                vetor[i] = (int) (Math.random() * vetor.length);
-                System.out.println(vetor[i]);
-            }
-            
-            long inicio = System.currentTimeMillis();
-            long fim;
-      
-            int aux;
+    public static void main(String[] args) {
+        
+        // Mudar o tamanho do vetor aqui (10.000, 50.000 ou 100.000)
+        int[] vetor = new int[50000];
+        
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = (int) (Math.random() * vetor.length);
+            System.out.println(vetor[i]);
+        }
+        
+        long inicio = System.currentTimeMillis();
+        long fim;
 
-            //Início so algoritmo do teste
+        int aux;
+
+        // Início do algoritmo do teste
+        
+        for (int i = 0; i < vetor.length - 1; i++) {
+            int minIndex = i;
             
-            for(int i = 0; i < vetor.length; i++){ 
-                for(int j = 0; j < vetor.length - 1 ; j++){ 
-                    if (vetor[j] > vetor[j+1]){
-                        aux = vetor[j];
-                        vetor[j] = vetor[j+1];
-                        vetor[j+1] = aux;
-                    }
+            for (int j = i + 1; j < vetor.length; j++) {
+                if (vetor[j] < vetor[minIndex]) {
+                    minIndex = j;
                 }
             }
-
-            //Fim do algoritmo do teste
             
-            System.out.println("Vetor ordenado");
-            for(int i = 0; i < vetor.length; i++){
-                System.out.println(vetor[i]);
+            if (minIndex != i) {
+                aux = vetor[i];
+                vetor[i] = vetor[minIndex];
+                vetor[minIndex] = aux;
             }
-
-            System.out.println();
-            
-            fim = System.currentTimeMillis();
-            System.out.println("Tamanho do vetor: " + (vetor.length));
-            System.out.println("Tempo em milisegundos: " + (fim-inicio));
         }
+        
+        // Fim do algoritmo do teste
+        
+        System.out.println("Vetor ordenado");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println(vetor[i]);
+        }
+
+        System.out.println();
+        
+        fim = System.currentTimeMillis();
+        System.out.println("Tamanho do vetor: " + (vetor.length));
+        System.out.println("Tempo em milisegundos: " + (fim - inicio));
     }
+}
