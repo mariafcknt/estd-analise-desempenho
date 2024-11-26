e8a6cc96918799a1758515e3dadcb26d802eb979
Guilherme Raiser <105473776+GuilhermeRaiser@users.noreply.github.com>
2024-11-26 22:39:40 +0000

    shell-sort

--- Main.java
+++ Main.java
@@ -1,43 +1,48 @@
 public class Main {
 
-        public static void main(String[] args) {
-            
-            //Mudar o tamanho do vetor aqui (10.000, 50.000 ou 100.000)
-            int[] vetor = new int[100000];
-            
-            for(int i = 0; i < vetor.length; i++){
-                vetor[i] = (int) (Math.random() * vetor.length);
-                System.out.println(vetor[i]);
-            }
-            
-            long inicio = System.currentTimeMillis();
-            long fim;
-      
-            int aux;
-
-            //Início so algoritmo do teste
-            
-            for(int i = 0; i < vetor.length; i++){ 
-                for(int j = 0; j < vetor.length - 1 ; j++){ 
-                    if (vetor[j] > vetor[j+1]){
-                        aux = vetor[j];
-                        vetor[j] = vetor[j+1];
-                        vetor[j+1] = aux;
-                    }
-                }
-            }
-
-            //Fim do algoritmo do teste
-            
-            System.out.println("Vetor ordenado");
-            for(int i = 0; i < vetor.length; i++){
-                System.out.println(vetor[i]);
-            }
-
-            System.out.println();
-            
-            fim = System.currentTimeMillis();
-            System.out.println("Tamanho do vetor: " + (vetor.length));
-            System.out.println("Tempo em milisegundos: " + (fim-inicio));
+    public static void main(String[] args) {
+        
+        // Mudar o tamanho do vetor aqui (10.000, 50.000 ou 100.000)
+        int[] vetor = new int[50000];
+        
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = (int) (Math.random() * vetor.length);
+            System.out.println(vetor[i]);
         }
+        
+        long inicio = System.currentTimeMillis();
+        long fim;
+
+        int aux;
+
+        // Início do algoritmo do teste
+        
+        int n = vetor.length;
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i++) {
+                int temp = vetor[i];
+                int j = i;
+                
+                // Mover os elementos do vetor[0..i-gap] que são maiores que o temp
+                while (j >= gap && vetor[j - gap] > temp) {
+                    vetor[j] = vetor[j - gap];
+                    j -= gap;
+                }
+                vetor[j] = temp;
+            }
+        }
+        
+        // Fim do algoritmo do teste
+        
+        System.out.println("Vetor ordenado");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println(vetor[i]);
+        }
+
+        System.out.println();
+        
+        fim = System.currentTimeMillis();
+        System.out.println("Tamanho do vetor: " + (vetor.length));
+        System.out.println("Tempo em milisegundos: " + (fim - inicio));
     }
+}
